d9fb52f66d2d53866adbbf939daaecf6a46e830b
nathanolszowski <olszowski.nathan@outlook.fr>
2025-07-23 16:58:45 +0200

    UP (config) typing
    adjusting filters out of the config

--- config/scrapers_config.py
+++ config/scrapers_config.py
@@ -23,17 +23,7 @@ class ScraperConf(TypedDict):
     scraper_type: ScraperType
     url_strategy: URLType
     start_link: Union[str, Dict[str, str]]
-    filters: Dict[str, Dict[str, str]]
 
-
-DEFAULT_FILTERS = {
-    "surface": {"field": "actif", "value": ["bureau"], "mode": "equal"},
-    "localisation": {
-        "field": "localisation",
-        "value": ["idf", "paris"],
-        "mode": "contains",
-    },
-}
 
 SCRAPER_CONFIG: Dict[str, ScraperConf] = {
     "BNP": {
@@ -42,37 +32,34 @@ SCRAPER_CONFIG: Dict[str, ScraperConf] = {
         "url_strategy": URLType.XML,
         "start_link": {
             "Bureaux": "https://www.bnppre.fr/sitemaps/bnppre/sitemap-bureaux.xml",
+            "Entrepot": "https://www.bnppre.fr/sitemaps/bnppre/sitemap-entrepots.xml",
+            "Activite": "https://www.bnppre.fr/sitemaps/bnppre/sitemap-locaux.xml",
             "Coworking": "https://bnppre.fr/sitemaps/bnppre/sitemap-coworking.xml",
         },
-        "filters": DEFAULT_FILTERS,
     },
     "JLL": {
         "enabled": False,
         "scraper_type": ScraperType.PW,
         "url_strategy": URLType.XML,
         "start_link": "https://immobilier.jll.fr/sitemap-properties.xml",
-        "filters": DEFAULT_FILTERS,
     },
     "CBRE": {
         "enabled": True,
         "scraper_type": ScraperType.HTTP,
         "url_strategy": URLType.XML,
         "start_link": "https://immobilier.cbre.fr/sitemap.xml",
-        "filters": DEFAULT_FILTERS,
     },
     "ALEXBOLTON": {
         "enabled": True,
         "scraper_type": ScraperType.HTTP,
         "url_strategy": URLType.XML,
         "start_link": "https://immobilier.cbre.fr/sitemap.xml",
-        "filters": DEFAULT_FILTERS,
     },
     "CUSHMAN": {
         "enabled": True,
         "scraper_type": ScraperType.HTTP,
         "url_strategy": URLType.XML,
         "start_link": "https://immobilier.cushmanwakefield.fr/sitemap.xml",
-        "filters": DEFAULT_FILTERS,
     },
     "KNIGHTFRANK": {
         "enabled": True,
@@ -82,24 +69,24 @@ SCRAPER_CONFIG: Dict[str, ScraperConf] = {
             "Location": "https://www.knightfrank.fr/resultat?nature=1&localisation=75%7C77%7C78%7C91%7C92%7C93%7C94%7C95%7C&typeOffre=1",
             "Vente": "https://www.knightfrank.fr/resultat?nature=2&localisation=75%7C77%7C78%7C91%7C92%7C93%7C94%7C95%7C&typeOffre=1",
         },
-        "filters": DEFAULT_FILTERS,
     },
     "ARTHURLOYD": {
         "enabled": True,
         "scraper_type": ScraperType.HTTP,
         "url_strategy": URLType.XML,
         "start_link": "https://www.arthur-loyd.com/sitemap-offer.xml",
-        "filters": DEFAULT_FILTERS,
     },
     "SAVILLS": {
         "enabled": True,
         "scraper_type": ScraperType.API,
         "url_strategy": URLType.API,
         "start_link": {
-            "Bureaux_Location": "/fr/fr/liste?Tenure=GRS_T_R&SortOrder=SO_PCDD&Currency=EUR&Period=Year&CommercialPropertyType=GRS_CPT_O&Receptions=-1&ResidentialSizeUnit=SquareMeter&CommercialSizeUnit=SquareMeter&LandAreaUnit=Acre&SaleableAreaUnit=SquareMeter&AvailableSizeUnit=SquareMeter&Category=GRS_CAT_COM&Shapes=W3sidHlwZSI6IkNpcmNsZSIsImNvb3JkaW5hdGVzIjpbMS40NDQyMDksNDMuNjA0NjUyXSwicmFkaXVzIjoiMjc4NzgwbSIsImxvY2F0aW9uSWQiOiI2MDcifV0",
-            "Bureaux_Vente": "/fr/fr/liste?Tenure=GRS_T_B&SortOrder=SO_PCDD&Currency=EUR&Period=Year&CommercialPropertyType=GRS_CPT_O&Receptions=-1&ResidentialSizeUnit=SquareMeter&CommercialSizeUnit=SquareMeter&LandAreaUnit=Acre&SaleableAreaUnit=SquareMeter&AvailableSizeUnit=SquareMeter&Category=GRS_CAT_COM&Shapes=W3sidHlwZSI6IkNpcmNsZSIsImNvb3JkaW5hdGVzIjpbMS40NDQyMDksNDMuNjA0NjUyXSwicmFkaXVzIjoiMjc4NzgwbSIsImxvY2F0aW9uSWQiOiI2MDcifV0",
+            "Bureaux_Location": "/fr/fr/liste?SearchList=Id_16+Category_RegionCountyCountry&Tenure=GRS_T_R&SortOrder=SO_PCDD&Currency=EUR&Period=Year&CommercialPropertyType=GRS_CPT_O&Receptions=-1&CommercialSizeUnit=SquareMeter&LandAreaUnit=SquareMeter&AvailableSizeUnit=SquareMeter&Category=GRS_CAT_COM&Shapes=W10",
+            "Bureaux_Vente": "/fr/fr/liste?SearchList=Id_16+Category_RegionCountyCountry&Tenure=GRS_T_B&SortOrder=SO_PCDD&Currency=EUR&Period=Year&CommercialPropertyType=GRS_CPT_O&Receptions=-1&ResidentialSizeUnit=SquareMeter&CommercialSizeUnit=SquareMeter&LandAreaUnit=Acre&SaleableAreaUnit=SquareMeter&AvailableSizeUnit=SquareMeter&Category=GRS_CAT_COM&Shapes=W10",
+            "Entrepots_Location": "/fr/fr/liste?SearchList=Id_1234+Category_RegionCountyCountry&Tenure=GRS_T_R&SortOrder=SO_PCDD&Currency=EUR&Period=Year&CommercialPropertyType=GRS_CPT_I&Receptions=-1&CommercialSizeUnit=SquareMeter&Category=GRS_CAT_COM&Shapes=W10",
+            "Entrepots_Vente": "/fr/fr/liste?SearchList=Id_1234+Category_RegionCountyCountry&Tenure=GRS_T_B&SortOrder=SO_PCDD&Currency=EUR&Period=Year&CommercialPropertyType=GRS_CPT_I&Receptions=-1&ResidentialSizeUnit=SquareMeter&CommercialSizeUnit=SquareMeter&LandAreaUnit=Acre&SaleableAreaUnit=SquareMeter&AvailableSizeUnit=SquareMeter&Category=GRS_CAT_COM&Shapes=W10",
+            "Coworking": "/fr/fr/liste?SearchList=Id_16+Category_RegionCountyCountry&Tenure=GRS_T_R&SortOrder=SO_PCDD&Currency=EUR&Period=Year&CommercialPropertyType=GRS_CPT_SO&Receptions=-1&CommercialSizeUnit=SquareFeet&LandAreaUnit=SquareFeet&AvailableSizeUnit=SquareFeet&Category=GRS_CAT_COM&Shapes=W10",
         },
-        "filters": DEFAULT_FILTERS,
     },
     # Add scrapers below
 }
